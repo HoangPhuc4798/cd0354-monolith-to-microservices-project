83a8809a4ff1a4dcc86d5efbf9c4a7871cf9be57
unknown <hoangphuc4798@gmail.com>
2024-12-28 00:57:30 +0700

    update screenshot

--- deployment/script/apply.sh
+++ deployment/script/apply.sh
@@ -11,14 +11,14 @@ kubectl apply -f ../backend-user-deployment.yml
 kubectl apply -f ../frontend-deployment.yml
 kubectl apply -f ../reverseproxy-deployment.yml
 
-# Apply Services
+# # Apply Services
 kubectl apply -f ../backend-feed-service.yml
 kubectl apply -f ../backend-user-service.yml
 kubectl apply -f ../frontend-service.yml
 kubectl apply -f ../reverseproxy-service.yml
 
 # Expose services
-#kubectl expose deployment frontend --type=LoadBalancer --name=pub-fe
-#kubectl expose deployment reverseproxy --type=LoadBalancer --name=pub-rp
+kubectl expose deployment frontend --type=LoadBalancer --name=pub-fe
+kubectl expose deployment reverseproxy --type=LoadBalancer --name=pub-rp
 
 echo 'Done!'
---
+++ deployment/script/clean_eks.sh
@@ -0,0 +1,2 @@
+./clean.sh
+aws eks delete-cluster --name hoangphuc-proj3
